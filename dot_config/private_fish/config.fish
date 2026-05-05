da361d3be42f947ad7ffae0f6558f933a06a3d64
Aleksei Sheludchenkov <aleshkashell@gmail.com>
2026-05-05 20:34:16 +0300

    Initial commit

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    bind ctrl-space accept-autosuggestion
+end
+### bling.fish source start
+test -f /usr/share/bazzite-cli/bling.fish && source /usr/share/bazzite-cli/bling.fish
+### bling.fish source end
