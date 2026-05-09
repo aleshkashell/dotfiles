3907b89ad4d607f447df8f3f461d3ecb7ca7ea40
Aleksei Sheludchenkov <aleshkashell@gmail.com>
2026-05-09 15:22:45 +0300

    Setup cli tooling

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     bind ctrl-space accept-autosuggestion
+    [ "$(command -v atuin)" ] && eval "$(atuin init fish)"
+    [ "$(command -v zoxide)" ] && eval "$(zoxide init fish)"
 end
 ### bling.fish source start
 test -f /usr/share/bazzite-cli/bling.fish && source /usr/share/bazzite-cli/bling.fish
